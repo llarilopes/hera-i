7882deedaa65451cefbeea7d0a8101d95fa24e9e
Felipe Machado Goose <lipegoose@gmail.com>
2025-04-27 10:55:16 -0300

    pré deploy backend automático

--- config/mail.php
+++ config/mail.php
@@ -9,15 +9,15 @@ return [
     | Supported: "smtp", "sendmail", "mailgun", "ses", "postmark", "log"
     |
     */
-    'driver' => env('MAIL_MAILER', 'smtp'),
-    'host' => env('MAIL_HOST'),
-    'port' => env('MAIL_PORT'),
+    'driver' => env('MAIL_MAILER', 'sendmail'),
+    'host' => env('MAIL_HOST', 'localhost'),
+    'port' => env('MAIL_PORT', 25),
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS'),
         'name' => env('MAIL_FROM_NAME'),
     ],
-    'encryption' => env('MAIL_ENCRYPTION'),
-    'username' => env('MAIL_USERNAME'),
-    'password' => env('MAIL_PASSWORD'),
+    'encryption' => env('MAIL_ENCRYPTION', ''),
+    'username' => env('MAIL_USERNAME', ''),
+    'password' => env('MAIL_PASSWORD', ''),
     'sendmail' => '/usr/sbin/sendmail -bs',
 ];
