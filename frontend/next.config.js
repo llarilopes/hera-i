453028e7698f4342ac94740be673599be681bb04
Felipe Machado Goose <lipegoose@gmail.com>
2025-04-27 08:38:20 -0300

    configurando url para API do Backend no frontend.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
 
 module.exports = {
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NODE_ENV === 'production'
+      ? 'https://api.hera-i.com.br'
+      : 'http://localhost:8000',
+  },
   output: 'export',
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
