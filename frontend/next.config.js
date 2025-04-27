8895ae353820cd93b9e29fb3d5f494f27bff44b9
Felipe Machado Goose <lipegoose@gmail.com>
2025-04-27 05:29:03 -0300

    ajustes finais no frontend.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // <- ISSO FAZ O Next EXPORTAR COMO SITE ESTÁTICO!
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
   },
 };
+
+module.exports = nextConfig;
