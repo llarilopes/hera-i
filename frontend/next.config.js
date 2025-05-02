ea1a68a0e16511ca80a838c2664991ecd97f0176
Felipe Machado Goose <lipegoose@gmail.com>
2025-05-02 15:33:10 -0300

    pós build para deploy frontend automático

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,17 +2,13 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 module.exports = {
+  output: 'export',  // Isso vai gerar a pasta 'out'
   images: {
     unoptimized: true,
   },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
-  },
-  // Configurações experimentais
-  experimental: {
-    // Manter swcMinify desativado, mas dentro de experimental
-    swcMinify: false
   },
   reactStrictMode: true,
 };
