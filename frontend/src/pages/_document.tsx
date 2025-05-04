c5f5e0dbab700b89b75634197688e009a2873c5f
Felipe Machado Goose <lipegoose@gmail.com>
2025-05-04 14:51:32 -0300

    configurado Meta Tags Sociais e Open Graph

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -11,6 +11,27 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
         <link rel="icon" href="/imgs/favicon.ico" type="image/x-icon" />
+        {/* Meta Tags Sociais e Open Graph */}
+        {/* Open Graph padrão */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://hera-i.com.br" />
+        <meta property="og:title" content="HeRa-i Agência inteligente" />
+        <meta property="og:description" content="Quase 30 anos de experiência em desenvolvimento web e sistemas digitais." />
+        <meta property="og:image" content="https://hera-i.com.br/imgs/open_graph_hera.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:site_name" content="HeRa-i" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content="https://hera-i.com.br" />
+        <meta name="twitter:title" content="HeRa-i Agência inteligente" />
+        <meta name="twitter:description" content="Quase 30 anos de experiência em desenvolvimento web e sistemas digitais." />
+        <meta name="twitter:image" content="https://hera-i.com.br/imgs/open_graph_hera.png" />
+
+        {/* Extras para SEO/social */}
+        <meta name="author" content="HeRa-i" />
+        <meta name="theme-color" content="#a3ff00" />
       </Head>
       <body className="antialiased">
         <Main />
